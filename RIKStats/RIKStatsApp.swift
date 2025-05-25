d332eadc376ff0fa7a29e813c406d1e77d4314de
AndreyVZav <zav.av@bk.ru>
2025-05-25 13:08:18 +0300

    Squashed commit of the following:
    commit 796e99382b3826b0fd493edeaed0f092549e9c78
    Author: AndreyVZav <zav.av@bk.ru>
    Date:   Sun May 25 12:36:36 2025 +0300
    
        start

---
+++ RIKStats/RIKStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RIKStatsApp.swift
+//  RIKStats
+//
+//  Created by Андрей Завадский on 25.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct RIKStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
